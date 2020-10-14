6163bab33ce335aeaa8f7b00ee626be70419eda9
supryantowp <supryantowp21@gmail.com>
2020-10-14 20:41:34 +0700

    test

--- src/pages/Map.jsx
+++ src/pages/Map.jsx
@@ -1,22 +1,50 @@
-import React, { useEffect } from 'react';
-import Layout from '../components/layouts';
-import Section from '../components/sections/Section';
+import React from 'react';
 import mapboxgl from 'mapbox-gl';
+import '../stylesheets/map.css';
 
 mapboxgl.accessToken =
   'pk.eyJ1Ijoic3VwcnlhbnRvaGVoZSIsImEiOiJja2c2ZDdvb3AxMjloMnNxcnRkZGc0NmRvIn0.mUDv0EX90oC4H_1H1vVFGQ';
 
-const Map = () => {
-  const state = {
-    lng: 5,
-    lat: 34,
-    zoom: 2,
-  };
-  return (
-    <Layout>
-      <Section> </Section>
-    </Layout>
-  );
-};
+class Map extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      lng: 5,
+      lat: 34,
+      zoom: 2,
+    };
+  }
+
+  componentDidMount() {
+    const map = new mapboxgl.Map({
+      container: this.mapContainer,
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [this.state.lng, this.state.lat],
+      zoom: this.state.zoom,
+    });
+
+    map.on('move', () => {
+      this.setState({
+        lng: map.getCenter().lng.toFixed(4),
+        lat: map.getCenter().lat.toFixed(4),
+        zoom: map.getZoom().toFixed(2),
+      });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="sidebarStyle">
+          <div>
+            Longitude: {this.state.lng} | Latitude: {this.state.lat} | Zoom:{' '}
+            {this.state.zoom}
+          </div>
+        </div>
+        <div ref={el => (this.mapContainer = el)} className="mapContainer" />
+      </div>
+    );
+  }
+}
 
 export default Map;
