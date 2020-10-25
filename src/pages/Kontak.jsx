8ba9e2d1832b93af94facb7a170b01e5e511294c
SYAUQIZAIDAN KHAIRAN KHALAF <syaokay@gmail.com>
2020-10-26 02:36:14 +0700

    [Completed ✔] Contract Section

--- src/pages/Kontak.jsx
+++ src/pages/Kontak.jsx
@@ -35,41 +35,60 @@ const Kontak = () => {
           <div className="col-md-12 mt-2" style={{ fontSize: 16 }}>
             {' '}
             Hubungi kami sekarang untuk mengenal lebih jauh Parawisely bisa
-            membantu kamu dalam tempat wisata, virtual tour dan liburan di
-            Indonesia.
+            membantu kamu dalam{' '}
+            <b> tempat wisata, virtual tour dan liburan di Indonesia.</b>
           </div>
         </div>
         <div className="row mt-5">
           <div className="col-md-5">
-            <FormInput id="nama_lengkap" type="name" label="Nama Lengkap" />
-            <FormInput id="email" type="email" label="Email" />
-            <Text>Apa pesanmu?</Text>
-            <Textarea placeholder="Here is a sample placeholder" />
-            <Button colorScheme="blue">Submit</Button>
+            <FormControl className="mt-3" id="nama_lengkap">
+              <FormLabel>Nama Lengkap</FormLabel>
+              <Input type="name" />
+            </FormControl>
+            <FormControl className="mt-3" id="email">
+              <FormLabel>Email</FormLabel>
+              <Input type="email" />
+            </FormControl>
+            <FormControl className="mt-3" id="pesan">
+              <FormLabel>Apa pesanmu?</FormLabel>
+              <Textarea placeholder="Here is a sample placeholder" />
+            </FormControl>
+            <Button colorScheme="blue" px="4rem" className="mt-3">
+              Submit
+            </Button>
           </div>
           <div className="col-md-7">
             <p className="font-weight-bold" style={{ fontSize: 24 }}>
-              Ask How We Can Help You:
+              Tanya bagaimana cara kami membantu anda:
             </p>
             <div className="mt-3">
-              <p>See our platform in action</p>
               <p>
-                Request a personalized demo of Fellow, or sign up for a free
-                account.
+                {' '}
+                <b> Bagaimana cara memakai fitur Parawisely</b>
+              </p>
+              <p>
+                Anda bisa menjelajahi fitur eksplorasi, wisata daerah dan fitur
+                virtual tour untuk menikmati fitur Parawisely.
               </p>
             </div>
             <div className="mt-3">
-              <p>See our platform in action</p>
               <p>
-                Request a personalized demo of Fellow, or sign up for a free
-                account.
+                {' '}
+                <b> Jika anda merupakan pengelola tempat pariwisata </b>
+              </p>
+              <p>
+                Jika tempat anda tidak terdaftar di Database kami, anda dapat
+                memasukan nya secara manual dengan fitur mitra pariwisata.
               </p>
             </div>
             <div className="mt-3">
-              <p>See our platform in action</p>
               <p>
-                Request a personalized demo of Fellow, or sign up for a free
-                account.
+                {' '}
+                <b> Mengalami kesalahan dan bug </b>
+              </p>
+              <p>
+                Anda dapat menghubungi kami dengan mengisi form di sebelah kiri
+                anda, lalu akan kami balas secepatnya.
               </p>
             </div>
           </div>
