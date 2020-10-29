8f0db64277ea96d4fb6285dc69df954824296b10
SYAUQIZAIDAN KHAIRAN KHALAF <syaokay@gmail.com>
2020-10-30 03:35:54 +0700

    Adding search bar

--- src/pages/WisataDaerah.jsx
+++ src/pages/WisataDaerah.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { useQuery } from 'react-query';
-import { Heading, Icon, Image, Stack, Text } from '@chakra-ui/core';
+import { Heading, Icon, Image, Stack, Text, Button } from '@chakra-ui/core';
 import Layout from '../components/layouts';
 import Section from '../components/sections/Section';
 import Banner from '../components/sections/Banner';
 import { BsClock, BsBookmarks, BsEye } from 'react-icons/bs';
+import { MdPlace } from 'react-icons/md';
 import Spinner from '../components/ui/Spinner';
 
 import wisataDaerahImages from '../assets/images/wisata-daerah-images.png';
@@ -36,10 +37,14 @@ const WisataDaerah = () => {
                   <Stack
                     key={`alskdl-${index}`}
                     direction="row"
-                    shadow="lg"
-                    borderRadius="md"
-                    p={8}
-                    spacing="2rem"
+                    shadow="
+ rgba(0, 0, 0, 0) 0px 0.9px 3.2px 0px, rgba(0, 0, 0, 0.004) 0px 2.1px 7.1px 0px, rgba(0, 0, 0, 0.004) 0px 3.5px 12.1px 0px, rgba(0, 0, 0, 0.008) 0px 5.4px 18.7px 0px, rgba(0, 0, 0, 0.01) 0px 8.1px 27.7px 0px, rgba(0, 0, 0, 0.016) 0px 11.8px 40.7px 0px, rgba(0, 0, 0, 0.02) 0px 17.7px 60.9px 0px, rgba(0, 0, 0, 0.03) 0px 28.3px 97.1px 0px, rgba(0, 0, 0, 0.07) 0px 53px 182px 0px
+
+"
+                    borderRadius="5px"
+                    p="30px"
+                    spacing="4rem"
+                    mb="3rem"
                   >
                     <Image
                       src={WisataDaerahIMG}
@@ -48,16 +53,13 @@ const WisataDaerah = () => {
                       borderRadius="md"
                     />
                     <Stack align="start" w="60%">
-                      <Heading fontSize="22px">{item.name}</Heading>
+                      <Heading fontSize="32px" fontWeight="extra_bold">{item.name}</Heading>
                       <Text>{item.description}</Text>
-                      <Link>Selengkapnya ...</Link>
+                         <Button colorScheme="blue" px="4rem">
+              Selengkapnya →
+            </Button> 
                     </Stack>
-                    <Stack>
-                      <Stack spacing="10px" align="center" direction="row">
-                        <Icon as={BsClock} />
-                        <Text>{item.places.length} Tempat</Text>
-                      </Stack>
-                    </Stack>
+                    
                   </Stack>
                 ))}
               </>
